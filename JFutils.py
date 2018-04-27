cd7a64c6094bae906ae4d4df523d49c3faf794eb
JeanFrancois Nadeau <nadj2315@ip29.m>
2018-04-27 14:15:45 -0400

    update 1: Modif progress_bar, add read_files & read_files modules

--- JFutils.py
+++ JFutils.py
@@ -1,18 +1,64 @@
-######################## PROGRESS BAR #############################
-def progress_bar(initial_time, current_count, total_count, bar_size=25):
-    """ This function print a dynamic progress bar on the terminal.
-        USAGE:  1) before your loop, save the time (time.time()) and the total number of elements/loops (ex: The size of the list you're looping on) 
-                2) Make sure you have a counter in your loop
-                3) Inside your loop, call this function with the 3 mandatory arguments
-                opt) You can change the size of the progress bar [==>      ] with the 4th argument. By default, it's 25 characters long
-                *warning: The display will not work properly if the bar is larger than your terminal window size """
+import time
+import os
 
-    if(bar_size<3 or bar_size>100):
-        print("ERROR: PROGRESS BAR DISPLAY FAILURE: bar_size must be between 3 and 100 inclusively", end='\r')
-        return
-    m=int((time.time() - initial_time)/60)
-    s=int((time.time() - initial_time)%60)
+###### Utilities #######
+def progress_bar(initial_time, total_count, current_count):
+    bar_size=10
+    t=time.time()
+    m=int((t-initial_time)/60)
+    s=int((t-initial_time)%60)
     percent=int((current_count/total_count*100))
-    progress="Progress: {}/{} ({}%) | Time elapsed: {}:{ss:02d}".format(current_count,total_count,percent,m,ss=s)
-    print("...[{}>{}] {}".format("="*int(percent/int(100/bar_size))," "*(bar_size-int(percent/int(100/bar_size))),progress), end='\r')
-###################################################################
+    if percent>100:
+        return
+    progress="{}/{} ({}%) | time: {}:{ss:02d}".format(current_count,total_count,percent,m,ss=s)
+    print("...[{}>{}] {}".format("="*int(percent*bar_size/100)," "*(bar_size-int(percent*bar_size/100)),progress), end='\r')
+
+
+###### Read files #######
+def read_config(filename):
+    configs={}
+    if os.path.isfile(filename):
+        with open(filename, 'r') as f:
+            lines=f.readlines()
+        for line in lines:
+            if not line.startswith('#'):
+                line=line.rstrip('\n').split('=')
+                configs[line[0]]=line[1]
+    return configs
+
+def read_tsv(filename):
+    if os.path.isfile(filename):
+        return read_file(filename,'\t')
+    else:
+        print(filename, "does not exists")
+def read_csv(filename):
+    if os.path.isfile(filename):
+        return read_file(filename,',')
+    else:
+        print(filename, "does not exists")
+
+def read_file(filename, separator):
+    with open(filename, 'r') as f:
+        lines=[line.rstrip('\n').split(separator) for line in f]
+    return lines
+
+
+###### Write files #######
+def write_tsv(item_list, filename):
+    if type(filename) is list:
+        return write_file(item_list, filename, '\t')
+    else:
+        print("input must be of type list")
+
+def write_csv(item_list, filename):
+    if type(filename) is list:
+        return write_file(item_list, filename, ',')
+    else:
+        print("input must be of type list")
+
+def write_file(item_list, filename, separator):
+    if item_list:
+        with open(filename, 'w') as f:
+            f.writelines(separator.join(i) + '\n' for i in item_list)
+    else:
+        print("The list is empty")
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # JFutils
+
+### requirements: python3 
+
+### How to use the modules:
+#####Option 1: 
+Have the script in your working directory and import JFutils
+#####Option 2:
+Add the script to your pythonpath
+PYTHONPATH="${PYTHONPATH}:<path/to>/JFutils/"
+export PYTHONPATH
+
+Note: add this to your bashrc to have it permanantly
+
+###Modules:
+#####progress_bar:
+This is a loop based progress bar. It prints a dynamic progress of the execution on the screen.
+Usage:
+1)Save the time with time.time() outside of the loop. (initial_time)
+2)Get the number of loops your script will do (total_count)
+3)Make sure you have a counter in your loop (current_count)
+4)Call the module with the 3 arguments above: progress_bar(initial_time, total_count, current_count)
+
+Note: The dynamic display will fail if the terminal size is smaller than the lenght of the progress bar line
+
+#####read_config:
+This module reads config files.
+example of a config file:
+\# This is a config file
+param1=value1
+paeam2=value2
+
+The module returns a dictionary {param1: value1, param2: value2}
+Usage: read_config(filename)
+
+#####read_tsv and read_csv:
+Those modules reads tab separated and coma separated files.
+They return a list of lines. Each lines is a list of columns.
+Usage: read_tsv(filename)
+example:
+file:
+a b c
+d e f
+g h i
+read_tsv(file) will return [['a', 'b', 'c']['d', 'e', 'f']['g', 'h', 'i']]
+
+#####write_tsv and write_csv:
+Those modules writes the informations of a list in a tab separated or coma separated format.
+The input must be a list of lists.
+Usage: write_csv(l)
+example:
+l=[['a', 'b', 'c']['d', 'e', 'f']['g', 'h', 'i']]
+write_csv(l) will produce a file containg this information:
+a,b,c
+d,e,f
+g,h,i
